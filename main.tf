f05654408fc84623f0e71986846e98cef320b838
HyderAli <mhyder9059@gmail.com>
2025-03-27 00:03:47 -0400

    Added basic Terraform EC2 configuration

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-provider "aws" { region = "us-east-1" }
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Change this to a valid AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
